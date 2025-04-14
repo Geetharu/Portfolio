65dacb0e401edd8f11f6647becb21f9ad0467306
Geetharu <geetharu89@gmail.com>
2025-04-14 22:18:06 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold text-pink-500">
+        Hello I'm Geeth!
+      </h1>
+    </main>
+  )
+}
